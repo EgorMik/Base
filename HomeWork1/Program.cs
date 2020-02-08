edcb2a6a5224ddd8db9a215f5e38f209e24e4207
EgorMik <56230522+EgorMik@users.noreply.github.com>
2020-02-08 21:42:21 +0300

    Number

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число, сумму цифр которого необходимо найти: ");
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+            int absInputNumber = Math.Abs(inputNumber);
+            int summ = 0;
+
+            while (absInputNumber > 0)
+            {
+                summ = summ + absInputNumber % 10;
+                absInputNumber = absInputNumber / 10;
+            }
+            Console.WriteLine($"Сумма цифр введенного числа равна: {summ}");
+            Console.ReadKey();
+        }
+    }
+}
