f967871f28e4e51e66b3f86d060cd6b5b2532d0d
EgorMik <56230522+EgorMik@users.noreply.github.com>
2020-02-09 01:25:03 +0300

    LINQ

---
+++ HomeWork14/GasStation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork14
+{
+    class GasStation
+    {
+        private class Fuel
+        {
+            public int FuelGrade { get; set; }
+            public double PriceForGallone { get; set; }
+        }
+        public enum CompanyList
+        {
+            Unknown,
+            Wawa,
+            QuikTrip,
+            Sheetz
+        }
+
+        public enum StreetList
+        {
+            Unknown,
+            Lincoln,
+            Freemont,
+            Portland
+        }
+
+
+        private Fuel fuel92 = new Fuel()
+        {
+            FuelGrade = 92,
+            PriceForGallone = 0.00
+        };
+
+        private Fuel fuel95 = new Fuel()
+        {
+            FuelGrade = 95,
+            PriceForGallone = 0.00
+        };
+
+        private Fuel fuel98 = new Fuel()
+        {
+            FuelGrade = 98,
+            PriceForGallone = 0.00
+        };
+
+        public StreetList Street { get; set; }
+        public double PriceForGallone92 { get => fuel92.PriceForGallone; set => fuel92.PriceForGallone = value; }
+        public double PriceForGallone95 { get => fuel95.PriceForGallone; set => fuel95.PriceForGallone = value; }
+        public double PriceForGallone98 { get => fuel98.PriceForGallone; set => fuel98.PriceForGallone = value; }
+        public CompanyList Company { get; set; }
+
+    }
+}
---
+++ HomeWork14/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<GasStation> gasStations = new List<GasStation>();
+            gasStations.Add(new GasStation()
+            {
+                Company = GasStation.CompanyList.QuikTrip,
+                Street = GasStation.StreetList.Lincoln,
+                PriceForGallone92 = 1.00,
+                PriceForGallone95 = 1.25,
+                PriceForGallone98 = 1.50
+            });
+
+            gasStations.Add(new GasStation()
+            {
+                Company = GasStation.CompanyList.Wawa,
+                Street = GasStation.StreetList.Lincoln,
+                PriceForGallone92 = 1.01,
+                PriceForGallone95 = 1.24,
+                PriceForGallone98 = 1.60
+            });
+
+            gasStations.Add(new GasStation()
+            {
+                Company = GasStation.CompanyList.Wawa,
+                Street = GasStation.StreetList.Freemont,
+                PriceForGallone92 = 0.99,
+                PriceForGallone95 = 1.20,
+                PriceForGallone98 = 1.50
+            });
+
+            gasStations.Add(new GasStation()
+            {
+                Company = GasStation.CompanyList.Sheetz,
+                Street = GasStation.StreetList.Freemont,
+                PriceForGallone92 = 1.09,
+                PriceForGallone98 = 1.41
+            });
+
+            gasStations.Add(new GasStation()
+            {
+                Company = GasStation.CompanyList.QuikTrip,
+                Street = GasStation.StreetList.Portland,
+                PriceForGallone92 = 0.80,
+                PriceForGallone95 = 1.10,
+                PriceForGallone98 = 1.25
+            });
+
+            gasStations.Add(new GasStation()
+            {
+                Company = GasStation.CompanyList.Sheetz,
+                Street = GasStation.StreetList.Portland,
+                PriceForGallone92 = 0.82,
+                PriceForGallone95 = 1.15,
+            });
+
+            gasStations.Add(new GasStation()
+            {
+                Company = GasStation.CompanyList.Wawa,
+                Street = GasStation.StreetList.Portland,
+                PriceForGallone92 = 0.99,
+                PriceForGallone95 = 1.2,
+                PriceForGallone98 = 1.6
+            });
+
+            var streets = gasStations.OrderBy(gasStation => gasStation.Street.ToString())
+                                    .GroupBy(gasStation => gasStation.Street)
+                                    .Select(street => new { Street = street.Key, NumberOfStations = street.Count() });
+
+            foreach (var street in streets)
+            {
+                Console.WriteLine($"On the {street.Street}-Street are {street.NumberOfStations} gas stations");
+            }
+
+        }
+    }
+}
+   
